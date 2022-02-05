c04e1a10ead10c869de022a2b453857a405eb024
oplopez <penelopeflores98@gmail.com>
2022-02-05 09:52:33 -0600

    Mi primer programa en c++

---
+++ programas c++/TAREA 1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a=0, b=0;
+	
+	cout<<"Ingrese el valor de a: ";
+	cin>>a;
+	
+	cout<<"Ingrese el valor de b: ";
+	cin>>b;
+	
+	cout<<"La suma de a + b es: "<<a+b<<endl;
+	cout<<"La resta de a - b es: "<<a-b<<endl;
+	cout<<"La multiplicacion de a * b es: "<<a*b<<endl;
+	cout<<"La division de a / b es: "<<a/b<<endl;
+	
+	system("pause");
+	return 0;
+	
+}
+

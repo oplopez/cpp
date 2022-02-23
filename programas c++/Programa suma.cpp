36200daa65d70d9afdb08f263a3190fffa5e3461
López <98791935+oplopez@users.noreply.github.com>
2022-02-23 12:06:34 -0600

    Add files via upload

---
+++ programas c++/Programa suma.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int numero1, numero2, contador=0, suma = 0;
+
+cout<<" Ingrese el primer numero : \n"; 
+cin>>numero1;
+cout<<" Ingrese el segundo numero : \n"; 
+cin>>numero2;
+
+for (int contador=numero1, contador=numero2) {
+
+suma += contador;
+contador++;
+
+
+	
+cout<<"La suma de los dos numeros es : "<<suma;	
+
+
+		
+}
+
+return 0;
+
+}

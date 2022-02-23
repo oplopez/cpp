b9e26529808907e862bb4230c9199cd5d35ecfef
López <98791935+oplopez@users.noreply.github.com>
2022-02-23 12:17:41 -0600

    Add files via upload

---
+++ programas c++/Do While.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	int a = 2;
+	int b = 10;
+	
+	while(a > b) {
+		cout << "(while) a > b" << endl;
+		break;
+	}
+	
+	do {
+		cout << "(do while) a > b" << endl;
+		break;
+	} while (a > b);
+	
+	return 0;
+}
